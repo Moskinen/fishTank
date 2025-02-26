80f0878d9aaa8cf4d746aaa45ac001c68b1600a1
gitgud <jonathanbirkstrom@gmail.com>
2025-02-26 11:31:54 +0100

    poop

--- src/fishTank.java
+++ src/fishTank.java
@@ -1,2 +1,42 @@
+import java.util.Scanner;
+
 public class fishTank {
+    Scanner scanner = new Scanner(System.in);
+    private String lastWaterChange;
+    private String waterQuality;
+
+    public String waterPercent;
+    public String algeKiller;
+
+    enum waterQuality {
+        PERFECT,
+        GOOD,
+        OKAY,
+        BAD
+    }
+
+    public fishTank(String lastWaterChange, String waterQuality) {
+        this.lastWaterChange = lastWaterChange;
+        this.waterQuality = waterQuality;
+    }
+
+    public void changeWater(String waterPercent, String algeKiller) {
+        System.out.println("Whats the current date?");
+        String userInputDate = scanner.nextLine();
+
+        System.out.println("How many percent of the water would u like to change?");
+        String userInputPercent = scanner.nextLine();
+
+        System.out.println("How many ml of alge killer would u like to add?");
+        String userInputAlgeKiller = scanner.nextLine();
+
+
+    }
+
+    @Override
+    public String toString() {
+        return "fishTank{" +
+                "lastWaterChange='" + lastWaterChange + '\'' +
+                '}';
+    }
 }
--- src/testClass.java
+++ src/testClass.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
 public class testClass {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        fishTank fishtank = new fishTank("26-02-2025", "Good");
+
+        System.out.println("Welcome to our fish menu. \n You can select your option by choosing a number between 1 - 5");
+        System.out.println("1. Viewing fish status\n" +
+                "2. Updating fish health\n" +
+                "3. Changing water\n" +
+                "4. Viewing last water change\n" +
+                "5. Exiting");
+
+        while (true) {
+
+        String userInput = scanner.nextLine();
+
+        if (userInput.equals("1")) {
+
+        } else if (userInput.equals("2")) {
+
+        } else if (userInput.equals("3")) {
+            System.out.print("Would you like to change the water? Y/N");
+            String userInputWater = scanner.nextLine();
+            if (userInputWater.equals("Y")) {
+                fishtank.changeWater();
+            }
+        } else if (userInput.equals("4")) {
+            System.out.println(fishtank.toString());
+        } else if (userInput.equals("5")) {
+
+        }
+    }
+    }
+
 }
