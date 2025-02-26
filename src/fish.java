0d5d8accc59440bc23dfc7e49f28e3ef11899ddc
Yakoob1884 <vivja.aty@gmail.com>
2025-02-26 11:31:25 +0100

    Fish class

--- src/fish.java
+++ src/fish.java
@@ -1,2 +1,68 @@
 public class fish {
+    //Attributes
+    private final String name;
+    private String healthStatus;
+    private fishTank home;
+    private final String color;
+    private final String species;
+    private final boolean sex;
+
+    //Constructor
+    public fish(String name, fishTank home, String color, String species, boolean sex){
+        this.name = name;
+        this.healthStatus = "Healthy";
+        this.home = home;
+        this.color = color;
+        this.species = species;
+        this.sex = sex;
+    }
+
+    //Getter
+    String getName(){
+        return this.name;
+    }
+
+    String getHealth(){
+        return this.healthStatus;
+    }
+
+    fishTank getHome(){
+        return this.home;
+    }
+
+    String getColor(){
+        return this.color;
+    }
+
+    String getSpecies(){
+        return this.species;
+    }
+
+    String getSex(){
+        if(this.sex){
+            return "Female";
+        } else {
+            return "Male";
+        }
+    }
+
+    //Setter
+    void updateHealthStatus(String updateHealth){
+        this.healthStatus = updateHealth;
+    }
+
+    void changeHome(fishTank updateHome){
+        this.home = updateHome;
+    }
+
+
+    //toString
+    public String toString(){
+        return "Name of fish: " + name + "\nHealth status: " + healthStatus + " Lives in: "
+                + home + "\nFish info:\n" + color + ", " + species + ", " + this.getSex();
+    }
+
+
+
 }
+
